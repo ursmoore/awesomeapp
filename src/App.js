187b98148fcff2ee6a5186c534ca4f40a2648dc4
ursmoore <urs.moore@gmx.de>
2022-01-24 14:02:56 +0100

    fix warnings

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
+        <p>I hate coding</p>
+        {
+        
+        /* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,9 +18,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I hate coding
+          Learn React
         </a> */}
-        I hate coding
       </header>
     </div>
   );
